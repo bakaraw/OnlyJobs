0d12859aab8f21c2a355d506885a7cea1e95b5bd
linux bakaraw <bacalso.ton2x@gmail.com>
2024-09-22 16:16:51 +0800

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
