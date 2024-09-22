6a2015f6496725b6d8fe7dbc0f2f21f9bab7bfe5
linux bakaraw <bacalso.ton2x@gmail.com>
2024-09-22 11:52:41 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/u3L1J/only_job/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.u3L1J.only_job
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
