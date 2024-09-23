134e3001a3f277dc26a9dfc99238eac32fbf279a
linux bakaraw <bacalso.ton2x@gmail.com>
2024-09-23 11:03:44 +0800

    replaced sign in anonymously with sign in using email and password

--- lib/views/authenticate/authenticate.dart
+++ lib/views/authenticate/authenticate.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:only_job/views/authenticate/sign_in.dart';
+import 'package:only_job/views/authenticate/register.dart';
 
-class Authenticate extends StatelessWidget {
+class Authenticate extends StatefulWidget {
   const Authenticate({super.key});
 
   @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SignIn(),
-    );
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
   }
 }
