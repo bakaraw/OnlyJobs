1669a6486e4547afc9e335010fd69785b3f53fc5
LSLegend24 <160636682+LSLCOder@users.noreply.github.com>
2024-09-24 15:05:41 +0800

    register_login page

---
+++ lib/views/register_login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class RegisterLogin extends StatelessWidget {
+  const RegisterLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Image(image: image),
+            // Image(image: image),
+          ],
+        ),
+      ),
+    );
+  }
+}
