b0970a74afaf817ecca3f212a06f0a092eda5737
Leandro Paninsoro <paninsorolean@gmail.com>
2024-09-25 11:04:44 +0800

    add auth by laithon

--- lib/views/authenticate/sign_in.dart
+++ lib/views/authenticate/sign_in.dart
@@ -23,6 +23,82 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: emailctl,
+                validator: (val) =>
+                val == null || val.isEmpty ? 'Enter an email' : null,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passordctl,
+                obscureText: true,
+                validator: (val) => val == null || val.length < 6
+                    ? 'Enter password 6+ long'
+                    : null,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                  ),
+                  child: const Text(
+                    "Sign in",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState != null &&
+                        _formKey.currentState!.validate()) {
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          emailctl.text, passordctl.text);
 
+                      if (result == null) {
+                        setState(() {
+                          error = 'Invalid credentials';
+                        });
+                      }
+                    }
+                  }),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      widget.toggleView();
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: const Text("Register"),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
