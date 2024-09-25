1cd164b746f50ad00259b84207c0f10138da6766
De3moo <j.estrebello.535274@umindanao.edu.ph>
2024-09-26 00:40:36 +0800

    2nd.2

--- lib/views/authenticate/authenticate.dart
+++ lib/views/authenticate/authenticate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:only_job/views/authenticate/sign_in.dart';
+import 'package:only_job/views/authenticate/client_or_employee.dart';
 
 class Authenticate extends StatelessWidget {
   const Authenticate({super.key});
@@ -14,7 +15,13 @@ class Authenticate extends StatelessWidget {
             // Image(image: image),
             // Image(image: image),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ClientOrEmployee(),
+                  ),
+                );
+              },
               child: Text('Create New Account'),
             ),
             ElevatedButton(
--- lib/views/authenticate/client_or_employee.dart
+++ lib/views/authenticate/client_or_employee.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:only_job/views/authenticate/SignUpForm.dart';
 
 class ClientOrEmployee extends StatelessWidget {
-  ClientOrEmployee({super.key});
+  const ClientOrEmployee({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Choose Your Role'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => SignUpForm(),
+                  ),
+                );
+              },
+              child: Text('Employee'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Employer'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
