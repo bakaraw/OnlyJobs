58a1c53cb1990cd9a5ef041280b7b0449b40d535
LSLegend24 <160636682+LSLCOder@users.noreply.github.com>
2024-09-27 08:14:33 +0800

    1.5

--- lib/views/authenticate/client_or_employee.dart
+++ lib/views/authenticate/client_or_employee.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:only_job/views/authenticate/introduction.dart';
 import 'package:only_job/views/authenticate/sign_up_form.dart';
 
 class ClientOrEmployee extends StatelessWidget {
@@ -16,25 +17,55 @@ class ClientOrEmployee extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => SignUpForm(),
+            Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: 150,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => IntroPage(),
+                            ),
+                          );
+                        },
+                        child: Text('Employee'),
+                      ),
+                    ),
                   ),
-                );
-              },
-              child: Text('Employee'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+                ),
               ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Employer'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 80.0),
+            Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: 150,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Employer'),
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
--- lib/views/authenticate/register_or_login.dart
+++ lib/views/authenticate/register_or_login.dart
@@ -55,7 +55,7 @@ class RegisterLogin extends StatelessWidget {
                   );
                 },
                 child: Text(
-                  "Sign up",
+                  "Sign In",
                   style: TextStyle(
                     color: accent1,
                     fontWeight: FontWeight.bold,
