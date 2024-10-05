01f5823c2fbc2707faf27b0cadb4bb65f87b8678
linux bakaraw <bacalso.ton2x@gmail.com>
2024-10-05 08:33:59 +0800

    NavJS is replaced with SignUpForm
    - in the client_or_employee.dart, NavJs is replaced with SignUpForm

--- lib/views/authenticate/client_or_employee.dart
+++ lib/views/authenticate/client_or_employee.dart
@@ -60,7 +60,7 @@ class ClientOrEmployee extends StatelessWidget {
                         onPressed: () {
                           Navigator.of(context).push(
                             MaterialPageRoute(
-                              builder: (context) => NavJS(),
+                              builder: (context) => SignUpForm(),
                             ),
                           );
                         },
